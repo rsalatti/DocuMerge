c612a3970293832db207ff5875eef0b8979c3c01
Ryan Salatti <39781797+ryansalatti@users.noreply.github.com>
2018-06-13 00:51:58 -0700

    Delete auth.py

--- auth.py
+++
@@ -1,8 +0,0 @@
-import gspread
-from apiclient.discovery import build
-from oauth2client.service_account import ServiceAccountCredentials
-
-# use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
